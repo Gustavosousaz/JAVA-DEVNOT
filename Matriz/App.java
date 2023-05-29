79e9814f19192bbd2a737292026fa7d01fa8be0b
Gustavosousaz <gbw2920@gmail.com>
2023-05-29 19:21:06 -0300

    29/05

---
+++ Matriz/App.java
@@ -0,0 +1,7 @@
+package Matriz;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Matriz/MatrizExemplo.java
@@ -0,0 +1,22 @@
+package Matriz;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MatrizExemplo {
+    Scanner sc = new Scanner(System.in);
+    Random rd = new Random();
+
+    public void exemplo1() {
+        // criar uma matriz 10x9, preencher, imprimir e manipular
+        // declarar matriz
+        int matriz[][] = new int[10][9];// 10 linhas x 9 colunas
+        // preencher a minha matriz
+        for (int i = 0; i < 10; i++) {// laço referente a primeira dimensão [10]
+            for (int j = 0; j < 9; j++) {// laçp referente a segunda dimensão [9]
+ 
+            }
+
+        }
+    }
+}

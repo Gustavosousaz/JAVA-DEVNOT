ff497048d1e3dda6128913c519cd06df126adf0a
Gustavosousaz <gbw2920@gmail.com>
2023-06-12 22:00:04 -0300

    1206

--- Sa2ProjetoBanco/App.java
+++ Sa2ProjetoBanco/App.java
@@ -2,6 +2,7 @@ package Sa2ProjetoBanco;
 
 import javax.swing.JOptionPane;
 
+import Sa2ProjetoBanco.Conta.Conta;
 import Sa2ProjetoBanco.Conta.ContaPF;
 import Sa2ProjetoBanco.Conta.ContaPJ;
 
@@ -10,36 +11,42 @@ import Sa2ProjetoBanco.Conta.ContaPJ;
  */
 public class App {
 
+    private static int contPJ;
+
     public static void main(String[] args) {
         //criar os vetores de objetos
+        String conta;
         ContaPF contasPf[] = new ContaPF[10];
         ContaPJ contasPj[] = new ContaPJ[10];
         //criar a aplicação do banco
         boolean aberta = true;
-        int contPF = 0;
-        int contaAtual = 0;
+        int PF = 0;
+        int PJ = 0;
         while(aberta){
             int acao = Integer.parseInt(JOptionPane.showInputDialog(
                 "Escolha a ção desejada:"
-                +"\n 1-Abrir Conta"
+                +"\n 1-Criar Conta"
                 +"\n 2-Acessar Conta"
                 +"\n 3-Sair"
             ));
-            if(acao ==1){//abrir conta PF
-                //criar a conta
-                //instanciar o objeto
-                contasPf[contPF] = new ContaPF();//objeto criado
+            if(acao ==1){
+                // Criar conta
+                conta = JOptionPane.showInputDialog("Deseja criar uma conta PF ou PJ: ");
                 //preencher as informações da conta
-                contasPf[contPF].setNome(JOptionPane.showInputDialog("Informe o Nome:"));
-                contasPf[contPF].setnCpf(JOptionPane.showInputDialog("Informe o CPF:"));
-                contasPf[contPF].setSaldo(0);
-                contasPf[contPF].setnConta(1000+contPF);
-                contPF++;
+                if (conta .equals("PF")){
+                    contasPj[PJ].setNome(JOptionPane.showInputDialog("Informe o Nome:"));
+                    contasPj[PJ].setnCnpj(JOptionPane.showInputDialog("Informe o CPF:"));
+                } else
+                contasPf[PF].setNome(JOptionPane.showInputDialog("Informe o Nome:"));
+                contasPf[PF].setnCpf(JOptionPane.showInputDialog("Informe o CPF:"));
+                // contasPf[PF].setSaldo(0);
+                // contasPf[PF].setnConta(1000+PF);
+                // PF++;
             }
             else if(acao==2){//buscar a conta já criada da PF
+                int nContaBusca = Integer.parseInt(JOptionPane.showInputDialog("Informe o nº da conta buscada:"));
                 for (int i = 0; i < contasPf.length; i++) {
                     //busca pelo nº da conta
-                    int nContaBusca = Integer.parseInt(JOptionPane.showInputDialog("Informe o nº da conta buscada:"));
                     if(nContaBusca == contasPf[i].getnConta()){
                         contaAtual = i;
                         JOptionPane.showMessageDialog(null,"Conta Encontrada");
@@ -47,9 +54,16 @@ public class App {
                 }
                 boolean acessar = true;
                 while(acessar){
-                    int acao2 = JOptionPane.showInputDialog("Ação desejada:"
+                    int acao2   = Integer.parseInt(JOptionPane.showInputDialog(
+                    "Ação desejada:"
                     +"\n 1- verificar saldo"
-                    +"\n 2-Saque")
+                    +"\n 2-Saque"));
+                    if(acao2 == 1){
+                    contasPf[contaAtual].getSaldo();
+                } else if (acao2 ==2){
+                    contasPf[contaAtual].saque();
+                }
+                                    
                 }
             }
 
--- Sa2ProjetoBanco/Conta/Conta.java
+++ Sa2ProjetoBanco/Conta/Conta.java
@@ -1,32 +1,57 @@
 package Sa2ProjetoBanco.Conta;
 
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 public abstract class Conta {
-    //atributos
+    Scanner sc = new Scanner(System.in);
+    // atributos
     String nome;
     int nConta;
     double saldo;
-    //set get
+
+    // set get
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public int getnConta() {
         return nConta;
     }
+
     public void setnConta(int nConta) {
         this.nConta = nConta;
     }
+
     public double getSaldo() {
         return saldo;
     }
+
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
+
+    // metado vazio
     public void saque() {
-        
-        saldo -=saque
+        System.out.println("Digite o valor do saque desejado!");
+        double saque = sc.nextDouble();
+        if (saque <= saldo) {
+            saldo -= saque;
+        } else {
+            System.out.println("Não foi possivel realizar o saque (SALDO INSUFICIENTE!)");
+        }
     }
-    
+
+    public void deposito() {
+        System.out.println("Digite o valor do deposito desejado:");
+        double deposito = sc.nextDouble();
+        saldo = saldo + deposito;
+        System.out.println("Você efetuou um deposito de: "+deposito + "seu saldo atual é  " +saldo);
+        
+    }
+
 }
